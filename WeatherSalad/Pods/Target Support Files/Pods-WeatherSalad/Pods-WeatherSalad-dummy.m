f3125901e67e1246b63217d6ade8baa2eb0ca19d
Andrew <escapeanaemia@gmail.com>
2020-11-11 14:40:34 +0900

    init project +RxSwift + RxCocoa + Sanpkit

---
+++ WeatherSalad/Pods/Target Support Files/Pods-WeatherSalad/Pods-WeatherSalad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeatherSalad : NSObject
+@end
+@implementation PodsDummy_Pods_WeatherSalad
+@end
---
+++ WeatherSalad/WeatherSaladTests/WeatherSaladTests.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherSaladTests.swift
+//  WeatherSaladTests
+//
+//  Created by Andrew Han on 2020/11/11.
+//
+
+import XCTest
+@testable import WeatherSalad
+
+class WeatherSaladTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
